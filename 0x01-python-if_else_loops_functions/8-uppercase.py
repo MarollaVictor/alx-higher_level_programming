ec2ee1940492a84910c834cbf154d853ecaaf6c7
MarollaVictor <marollavictorsedky@gmail.com>
2024-09-11 20:00:20 +0300

    numbers

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(char):
+    if ord(char) >= ord('a') and ord(char) <= ord('z'):
+        return True
+    else:
+        return False
+
+def uppercase(str):
+    for char in str:
+        print("{:c}"
+              .format(ord(char) if not islower(char) else ord(char) - 32), end="")
+        print("")
