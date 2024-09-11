8966142f1ce418724f680917d9cb93b523b0752e
MarollaVictor <marollavictorsedky@gmail.com>
2024-09-11 19:44:23 +0300

    numberss

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for x in range(10):
     for z in range(x, 10):
         if x < z:
             print("{:d}{:d}".format(x, z),
-                    end="\n" if x == 8 and z == 9 else ", ")
+                  end="\n" if x == 8 and z == 9 else ", ")
