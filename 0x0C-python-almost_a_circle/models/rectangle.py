36ec074eda77fa4bab88f386e41d22a9ca575b70
MarollaVictor <marollavictorsedky@gmail.com>
2024-10-25 18:22:22 +0300

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,100 +1,51 @@
 #!/usr/bin/python3
-"""
-Rectangle Module
-"""
+'''Rectangle class.'''
 from base import Base
 
+
 class Rectangle(Base):
-     """
-     Rectangle class and base subclass
-     """
-     def __init__(self, width, height, x=0, y=0, id=None):
-         """
-         Initializw a new rectangle
-         """
-         super().__init__(id)
+    '''Initializing rectangle class.'''
 
-         self.width = width
-         self.height = height
-         self.x = x
-         self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Constructor.'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-     @property
-     def width(self):
-         """
-         width getter
-         """
-         return self.__width
+    @property
+    def width(self):
+        '''Width of rectangle.'''
+        return self.__width
 
-     @width.setter
-     def width(self, value):
-         """
-         witdh setter
-         """
-         if not isinstance(value, int):
-             raise TypeError("width must be an integer")
-         if value < 0:
-             raise ValueError("width must be >= 0")
-         self.__width = value
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
-     @property
-     def height(self):
-         """
-         height getter
-         """
-         return self.__height
+    @property
+    def height(self):
+        '''Width of rectangle.'''
+        return self.__height
 
-     @height.setter
-     def height(self, value):
-         """
-         height setter
-         """
-         if not isinstance(value, int):
-             raise TypeError("height must be an integer")
-         if value < 0:
-             raise ValueError("height must be >= 0")
-         self.__height = value
+    @height.setter
+    def height(self, value):
+        self.__height = value
 
-     @property
-     def x(self):
-         """
-         x getter
-         """
-         return self.__x
+    @property
+    def x(self):
+        '''Width of rectangle.'''
+        return self.__x
 
-     @x.setter
-     def x(self, value):
-         """
-         x setter
-         """
-         if not isinstance(value, int):
-             raise TypeError("x must be an integer")
-         if value < 0:
-             raise ValueError("x must be >= 0")
-         self.__x = value
+    @x.setter
+    def x(self, value):
+        self.__x = value
 
-     @property
-     def y(self):
-         """
-         y getter
-         """
-         return self.__y
+    @property
+    def y(self):
+        '''Width of rectangle.'''
+        return self.__y
 
-     @y.setter
-     def y(self, value):
-         """
-         y setter
-         """
-         if not isinstance(value, int):
-             raise TypeError("y must be an integer")
-         if value < 0:
-             raise ValueError("y must be >= 0")
-         self.__y = value
-     def area(self):
-
-         """
-         area og rectangle equation
-         """
-         area = self.w * self.h
-
-         return area
+    @y.setter
+    def y(self, value):
+        self.__y = value
